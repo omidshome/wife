61c709e4e93fddacf6c99f0593aabf7a149f803a
omidshome@gmail.com <omidshome@gmail.com>
2023-12-29 01:04:08 +0330

    two images

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,35 @@ import Player from "./components/Player";
 import "./App.css";
 import Mermaid from "./assets/mermaid.jpg";
 import Mermaid2 from "./assets/mermaid2.jpg";
+import Mermaid3 from "./assets/mermaid3.jpg";
 
 function App() {
+  const [theImage, setTheImage] = useState(Mermaid);
+  const listOfImages = [Mermaid, Mermaid2, Mermaid3];
+  function slidImages() {
+    const currentImageIndex = listOfImages.indexOf(theImage);
+    const nextImageIndex = (currentImageIndex + 1) % listOfImages.length;
+    setTheImage(listOfImages[nextImageIndex]);
+  }
+
   return (
     <div>
       <div className="box">
         <div className="title">
-          <img src={Mermaid2} alt="Mansoureh Abdi" className="shape" />
+          <img
+            src={theImage}
+            alt="Mansoureh Abdi"
+            className="shape fade-in"
+            onClick={slidImages}
+          />
           <h2>Mansoureh Abdi</h2>
         </div>
-        <h5 style={{ fontStyle: "italic" }}>I love you so much my mermaid</h5>
+        <h3 style={{ fontStyle: "italic", marginTop: "2rem" }}>
+          I love you so much my mermaid
+        </h3>
+
+        <h3>Your husband,</h3>
+        <h3>Omid</h3>
         <h5 style={{ color: "red", fontSize: "2rem" }}>♥</h5>
 
         <Player />
